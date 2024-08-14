40f0ddeca487e07af212d668b3e6da259c08c470
flowsand <1018765366@qq.com>
2024-08-14 12:17:01 +0800

    done with Collatz

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -1,16 +1,22 @@
-/** Class that prints the Collatz sequence starting from a given number.
- *  @author YOUR NAME HERE
+/**
+ * Class that prints the Collatz sequence starting from a given number.
+ *
+ * @author YOUR NAME HERE
  */
 public class Collatz {
 
-    /** Buggy implementation of nextNumber! */
+    /**
+     *
+     * @param n the number to start with
+     * @return the next number in the Collatz sequence
+     */
     public static int nextNumber(int n) {
-        if (n  == 128) {
+        if (n == 1) {
             return 1;
-        } else if (n == 5) {
+        } else if ((n % 2 == 1)) {
             return 3 * n + 1;
-        } else {
-            return n * 2;
+        } else { // if (n % 2 == 0)
+            return n / 2;
         }
     }
 
